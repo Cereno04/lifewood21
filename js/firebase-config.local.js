470e601cd2099ac97b723cd26782d2b113ef6315
Marlon <marlon.royalty@gmail.com>
2025-08-04 14:53:04 +0800

    Configure Firebase with environment variables

--- js/firebase-config.local.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCOwd8kXjhNQmDKPXH98co_8fxsoaUd2q0",
-  authDomain: "lifewood-04.firebaseapp.com",
-  projectId: "lifewood-04",
-  storageBucket: "lifewood-04.firebasestorage.app",
-  messagingSenderId: "709112942157",
-  appId: "1:709112942157:web:c0e7239c4a151e885d5604"
-};
-//
